09c787712ece517102cc303f4c2eea84c072b2e8
Gibb-y <98590606+Gibb-y@users.noreply.github.com>
2026-01-19 11:41:07 +0100

    Add: spikes

---
+++ Entities/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using SEGame.Animations;
+using SEGame.Collisions;
+using SEGame.EC;
+using SEGame.EC.Components;
+using SEGame.Managers;
+
+namespace SEGame.Entities
+{
+    public class Spikes : Entity
+    {
+        public Spikes(Point location)
+        {
+            AddComponent(new Transform(location));
+            AddComponent(new EnemyCollider(new Rectangle(0, 0, 16, 16), Vector2.Zero));
+            AddComponent(new Animator());
+            AddComponent(new Physics());
+
+            var idleAnim = new Animation("idle", 8, AssetManager.Instance.GetTexture2D("spikes"));
+            idleAnim.AddFrame(new AnimationFrame(new Rectangle(0, 0, 16, 16)));
+
+            GetComponent<Animator>().AddAnimation(idleAnim);
+        }
+    }
+}
--- Scenes/Level2.cs
+++ Scenes/Level2.cs
@@ -14,11 +14,19 @@ namespace SEGame.Scenes
             var platform2 = new Platform(new Point(700, 500), new Rectangle(400, 600, 16 * 3, 16));
             var platform3 = new Platform(new Point(800, 400), new Rectangle(400, 600, 16 * 3, 16));
             var we = new WalkingEnemy(new Point(400, 600));
+            var spikes = new Spikes(new Point(350, 600));
+            var spikes2 = new Spikes(new Point(300, 600));
+            var spikes3 = new Spikes(new Point(650, 600));
+            var spikes4 = new Spikes(new Point(700, 600));
 
             _entityManager.AddEntity(_player);
             _entityManager.AddEntity(platform);
             _entityManager.AddEntity(platform2);
             _entityManager.AddEntity(platform3);
+            _entityManager.AddEntity(spikes);
+            _entityManager.AddEntity(spikes2);
+            _entityManager.AddEntity(spikes3);
+            _entityManager.AddEntity(spikes4);
             _entityManager.AddEntity(we);
         }
 
