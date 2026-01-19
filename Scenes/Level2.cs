2578f17b7e2472dd216f40b493e83812cb951d51
Gibb-y <98590606+Gibb-y@users.noreply.github.com>
2026-01-19 11:44:18 +0100

    Finish level 2

--- Scenes/Level2.cs
+++ Scenes/Level2.cs
@@ -13,6 +13,8 @@ namespace SEGame.Scenes
             var platform = new Platform(new Point(800, 600), new Rectangle(400, 600, 16 * 3, 16));
             var platform2 = new Platform(new Point(700, 500), new Rectangle(400, 600, 16 * 3, 16));
             var platform3 = new Platform(new Point(800, 400), new Rectangle(400, 600, 16 * 3, 16));
+            var platform4 = new Platform(new Point(1000, 300), new Rectangle(400, 600, 16 * 3, 16));
+            var goal = new Goal(new Point(980, 170));
             var we = new WalkingEnemy(new Point(400, 600));
             var spikes = new Spikes(new Point(350, 600));
             var spikes2 = new Spikes(new Point(300, 600));
@@ -23,6 +25,8 @@ namespace SEGame.Scenes
             _entityManager.AddEntity(platform);
             _entityManager.AddEntity(platform2);
             _entityManager.AddEntity(platform3);
+            _entityManager.AddEntity(platform4);
+            _entityManager.AddEntity(goal);
             _entityManager.AddEntity(spikes);
             _entityManager.AddEntity(spikes2);
             _entityManager.AddEntity(spikes3);
